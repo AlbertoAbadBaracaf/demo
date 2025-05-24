55199868ee13a9a57e0e46993ba7d1f9b2d7c10d
Alberto Arturo Abad Varela <122945401+AlbertoAbadBaracaf@users.noreply.github.com>
2025-05-24 12:46:59 -0600

    first commit

---
+++ src/main/java/com/siscon/demo/SisconAbadApplication.java
@@ -0,0 +1,13 @@
+package com.siscon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisconAbadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisconAbadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siscon/demo/SisconAbadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siscon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisconAbadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
